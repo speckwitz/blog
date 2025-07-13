92b949a2732b0b39c1df89c3bf309fb41d18caef
Michael <hakushigo@hotmail.com>
2025-07-13 10:47:29 +0000

    new more stuff in my porto

--- app/page.tsx
+++ app/page.tsx
@@ -27,15 +27,29 @@ export default function About() {
         </div>
 
         <div className="row my-1 g-3 project-items">
-          <div className="col-sm-12  col-md-6 col-lg-4">
-            <div className="card">
-              <div className="card-body">
-                <h5 className='fw-bold'>TSTH2 cabbage count software</h5>
+          <div className="col-sm-12 col-md-6">
+            <Link href={"/note/2025-06-11_Cabbage-Counting-Software"}>
+              <div className="card">
+                <div className="card-body">
+                  <h5 className='fw-bold'>TSTH2 cabbage count software</h5>
+                  <p>a cabbage counting system build for TSTH2 to speed up the work of cabbage harvest management by automate the counting process with Machine Learning based object detection technology.</p>
+                </div>
               </div>
-            </div>
+            </Link>
           </div>
 
-          <div className="col-sm-12  col-md-6 col-lg-4">
+          <div className="col-sm-12  col-md-6">
+            <Link href="/note/2025-06-11_Tarhilala_Waste_Bank_Management_Software">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className='fw-bold'>Tarhilala waste-bank management software</h5> 
+                  <p>A wastebank management system build for Tarhilala Waste Bank, a local waste bank initiative to digitify and simplify the management process</p>
+                </div>
+              </div>
+            </Link>
+          </div>
+
+          <div className="col-sm-12  col-md-6">
             <div className="card">
               <div className="card-body">
                 <h5 className='fw-bold'>Pharmacy stock management software</h5>
@@ -43,32 +57,18 @@ export default function About() {
             </div>
           </div>
 
-          <div className="col-sm-12  col-md-6 col-lg-4">
-            <div className="card">
-              <div className="card-body">
-                <h5 className='fw-bold'>A Simple web-based note</h5>
+          <div className="col-sm-12  col-md-6">
+            <Link href={"https://github.com/speckwitz/bulettin"}>
+              <div className="card">
+                <div className="card-body">
+                  <h5 className='fw-bold'>A Simple web-based note</h5>
+                  <p>A simple To-do list app to explore the technology of golang and the vercel integration with golang language.</p>
+                </div>
               </div>
-            </div>
+            </Link>
           </div>
 
-          <div className="col-sm-12  col-md-6 col-lg-4">
-            <div className="card">
-              <div className="card-body">
-                <h5 className='fw-bold'>Tarhilala waste-bank management software</h5> 
-              </div>
-            </div>
-          </div>
-
-          <div className="col-sm-12 col-md-6 col-lg-4">
-            <div className="card">
-              <div className="card-body">
-                <h5 className='fw-bold'>Private webfont host</h5> 
-              </div>
-            </div>
-          </div>
-
-        </div>        
-
+        </div>
       </div>
       <BottomBar/>
     </main>
